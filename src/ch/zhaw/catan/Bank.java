15778374a792d5524b5335de380c9acd4bd36dc3
braenyve <braenyve@students.zhaw.ch>
2020-11-17 15:05:50 +0100

    Updated class structure of SiedlerBoard

--- src/ch/zhaw/catan/Bank.java
+++ src/ch/zhaw/catan/Bank.java
@@ -1,37 +1,38 @@
 package ch.zhaw.catan;
+
 import java.util.Map;
 
 import ch.zhaw.catan.Config.Resource;
 
 /**
- * This class holds a datafield for the amount of cards, the bank
- * current has for every resource of a catan game. It has the following functions:
- * Check, if a player can make a trade with the bank and carries out a trade with the bank.
+ * This class holds a datafield for the amount of cards, the bank current has
+ * for every resource of a catan game. It has the following functions: Check, if
+ * a player can make a trade with the bank and carries out a trade with the
+ * bank.
+ * 
  * @author Meier Robin, Moser Nadine, Brändli Yves
  * @version 1.0
  *
  */
 
 public class Bank {
-	
+
 	private Map<Resource, Integer> amountOfResources;
-	
-	
+
 	public Bank() {
 		amountOfResources = Config.INITIAL_RESOURCE_CARDS_BANK;
 	}
-	
-	
+
 	/**
-	 * Checks, if there were 4 given cards and enough cards in the bank stock.
-	 * If so, returns true and deducts number of resource-cards from the bank.
+	 * Checks, if there were 4 given cards and enough cards in the bank stock. If
+	 * so, returns true and deducts number of resource-cards from the bank.
 	 * 
-	 * @param resourcegiven	The resource, the player wants to trade.
-	 * @param givencards The number of the cards, the player is giving.
+	 * @param resourcegiven  The resource, the player wants to trade.
+	 * @param givencards     The number of the cards, the player is giving.
 	 * @param resourcewanted The resource, the player wants to have.
 	 * @return true, if the trade for one card was successful.
 	 */
-	public boolean trade (Resource resourcegiven, int givencards, Resource resourcewanted) {
+	public boolean trade(Resource resourcegiven, int givencards, Resource resourcewanted) {
 		boolean tradesuccessful = false;
 		if (givencards == 4 && amountOfResources.get(resourcewanted) >= 1) {
 			tradesuccessful = true;
--- src/ch/zhaw/catan/SiedlerBoard.java
+++ src/ch/zhaw/catan/SiedlerBoard.java
@@ -1,8 +1,76 @@
 package ch.zhaw.catan;
 
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
+
 import ch.zhaw.catan.Config.Land;
 import ch.zhaw.hexboard.HexBoard;
 
 public class SiedlerBoard extends HexBoard<Land, String, String, String> {
+	
+	/**
+	 * Creates a new fix board, with fix resource 
+	 * fields and dice numbers for the fields for a siedler game.
+	 */
+	public void initializeBoard() {
+		
+	}
+	
+	/**
+	 * Checks if there are settlements around a resource field.
+	 * If so returns a Map with all the settlements for this field and their faction.
+	 * @param field The resource field, that needs to be checked, if there are settlements around it.
+	 * @return A Map with the current settlements and their faction for the specified resource field.
+	 */
+	public Map<Config.Faction, Integer> searchFieldSettlement (Point field) {
+		Map<Config.Faction, Integer> settlementsAroundField = new HashMap<>();
+		
+		
+		return settlementsAroundField;
+	}
 
+	/**
+	 * Checks, if a building ground for a new settlement is vaLid.
+	 * @param bulidingGround The point, where the Player wants to build a settlement.
+	 * @return true, if the building ground for the new settlement is valid.
+	 */
+	public boolean checkIfValidSettlementBuildingGround(Point bulidingGround) {
+		boolean buildingGroundValid = false;
+		
+		return buildingGroundValid;
+	}
+	
+	/**
+	 * Checks, if a building ground for a new settlement is vaLid.
+	 * @param start The point, where the Player wants to start his road.
+	 * @param end The point, where the Player wants to end his road.
+	 * @return true, if the building ground for the new street is valid.
+	 */
+	public boolean checkIfValidStreeBuildingGround(Point start, Point end) {
+		boolean buildingGroundValid = false;
+		
+		return buildingGroundValid;
+	}
+	
+	/**
+	 * Builds a new settlement at the specified point for the actual player.
+	 * @param buildingGround The point, where the player wants to build his new settlement.
+	 * @param faction The faction of the current player.
+	 */
+	public void buildSettlement (Point buildingGround, Config.Faction faction) {
+		
+	}
+	
+	/**
+	 * Builds a new street between the specified point for the actual player.
+	 * @param start The point, where the Player wants to start his road.
+	 * @param end The point, where the Player wants to end his road.
+	 * @param faction The faction of the current player.
+	 */
+	public void buildStreet (Point start, Point end, Config.Faction faction) {
+		
+	}
+
+	
 }

f0990603e0f98855238be6404d5b0323db2bfb51
meierr02 <meierr02@students.zhaw.ch>
2020-11-22 15:05:22 +0100

    Added lowerField labels

--- src/ch/zhaw/catan/SiedlerBoardTextView.java
+++ src/ch/zhaw/catan/SiedlerBoardTextView.java
@@ -1,12 +1,30 @@
 package ch.zhaw.catan;
 
+import java.awt.Point;
+import java.util.Map;
+
 import ch.zhaw.catan.Config.Land;
 import ch.zhaw.hexboard.HexBoardTextView;
+import ch.zhaw.hexboard.Label;
 
 public class SiedlerBoardTextView extends HexBoardTextView<Land, String, String, String> {
 
   public SiedlerBoardTextView(SiedlerBoard board) {
     super(board);
+    Map<Point, Integer> lowerFieldLabel = Config.getStandardDiceNumberPlacement();
+    for (Map.Entry<Point, Integer> e : lowerFieldLabel.entrySet()) {
+    	if(e.getValue() < 10) {
+    		char digit = Character.forDigit(e.getValue(), 16);
+    		setLowerFieldLabel(e.getKey(), new Label('0',digit));
+    	}
+    	else {
+    		char firstDigit = Character.forDigit(Integer.parseInt(Integer.toString(e.getValue()).substring(0,1)), 16);
+    		char secondDigit = Character.forDigit(Integer.parseInt(Integer.toString(e.getValue()).substring(1)), 16);
+    		setLowerFieldLabel(e.getKey(), new Label(firstDigit, secondDigit));
+    	}
+      }
+    
   }
+  
 
 }
